d9828e8368c13a28b38bcd7f73cf3204d7f5b92f
Jakub Stecko <jakub.stec01@gmail.com>
2023-01-11 15:21:08 +0100

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ticks-and-crosses"
---
+++ src/test/java/com/kodilla/ticksandcrosses/TicksAndCrossesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.ticksandcrosses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicksAndCrossesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
